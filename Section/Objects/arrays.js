69573a29133955656e7ca2a4b9fdf90b03a421c1
Ian Izaguirre <ian@izaguir.re>
2018-09-12 17:44:40 -0400

    Add: array section

---
+++ Section/Objects/arrays.js
@@ -0,0 +1,11 @@
+var person = [];
+
+person.name = 'Mrs. White';
+person.sex = 'male';
+var who = person.name;
+
+who; /*?*/
+person; /*?*/
+
+// All Non-Primitive values are Objects
+typeof person === 'object'; /*?*/
